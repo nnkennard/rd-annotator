44f947189c4c6ebec888373ee1fce7ecb5dfecd8
Neha Kennard <nnayak@cs.umass.edu>
2020-07-28 21:44:28 -0700

    basic django site working

---
+++ rdasite/qa_annotate/views.py
@@ -0,0 +1,54 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.template import loader
+from django.urls import reverse
+
+
+from .models import Choice, Question
+
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'qa_annotate/detail.html', {'question': question})
+    #return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'qa_annotate/results.html', {'question': question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'qa_annotate/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('qa_annotate:results', args=(question.id,)))
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('qa_annotate/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+    #latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #output = "Hello hello " + ', '.join([q.question_text for q in latest_question_list])
+    #return HttpResponse(output)
+    #return HttpResponse("Hello, world. You're at the polls index.")
+
+
+# Create your views here.
